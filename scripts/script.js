524c24e9e94923e36231c3eb31fffe270e6c628d
Jhonatas Santos <santosjh1@gmail.com>
2022-08-24 23:24:53 +0000

    Remove console colors

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,7 @@ let computerSelection = ''
 let playerSelection = ''
 let computerWins = 0
 let playerWins = 0
-const Log = {
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    underscore: "\x1b[4m",
-    blink: "\x1b[5m",
-    reverse: "\x1b[7m",
-    hidden: "\x1b[8m",
-    // Foreground (text) colors
-    fg: {
-      black: "\x1b[30m",
-      red: "\x1b[31m",
-      green: "\x1b[32m",
-      yellow: "\x1b[33m",
-      blue: "\x1b[34m",
-      magenta: "\x1b[35m",
-      cyan: "\x1b[36m",
-      white: "\x1b[37m",
-      crimson: "\x1b[38m"
-    },
-    // Background colors
-    bg: {
-      black: "\x1b[40m",
-      red: "\x1b[41m",
-      green: "\x1b[42m",
-      yellow: "\x1b[43m",
-      blue: "\x1b[44m",
-      magenta: "\x1b[45m",
-      cyan: "\x1b[46m",
-      white: "\x1b[47m",
-      crimson: "\x1b[48m"
-    }
-  };
+
 function getComputerChoice() {
     const choice = Math.random()
     if (choice < 0.33) {
@@ -82,14 +50,14 @@ function game() {
             computerSelection = getComputerChoice()
             playerSelection = prompt('Choose your weapon:')
             if (playRound(computerSelection, playerSelection) === 'Tie') {
-                console.log(Log.fg.yellow,`${computerSelection} vs ${playerSelection}\n It's a tie.`,Log.reset)
-                console.log(Log.fg.cyan,score(0,0),Log.reset)
+                console.log(`${computerSelection} vs ${playerSelection}\n It's a tie.`)
+                console.log(score(0,0))
             } else if (playRound(computerSelection, playerSelection) === 'Computer') {
-                console.log(Log.fg.red,`${computerSelection} vs ${playerSelection}\n You lose the battle`,Log.reset)
-                console.log(Log.fg.cyan,score(1,0),Log.reset)
+                console.log(`${computerSelection} vs ${playerSelection}\n You lose the battle`)
+                console.log(score(1,0))
             } else if (playRound(computerSelection, playerSelection) === 'Player') {
-                console.log(Log.fg.green,`${computerSelection} vs ${playerSelection}\n You won the battle`,Log.reset)
-                console.log(Log.fg.cyan,score(0,1),Log.reset)
+                console.log(`${computerSelection} vs ${playerSelection}\n You won the battle`)
+                console.log(score(0,1))
             }
         }
     }
